1d905d8d8b25aa17e315ba362f4572a2d3330bee
horup <sah@sah.dk>
2020-01-16 21:51:09 +0100

    added svg stick man for testing

--- src/Avatar/index.tsx
+++ src/Avatar/index.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
-
+declare var require;
+let s = require("../../assets/test.svg");
 export const Avatar = ()=>
 {
+    let obj:HTMLObjectElement;
+    let svg: SVGSVGElement;
+    React.useEffect(()=>
+    {
+        obj.onload = ()=>{
+            svg = obj.contentDocument.firstElementChild as SVGSVGElement;
+            console.log(svg);
+            let head = svg.getElementById("head");
+            let shirt = svg.getElementById("shirt");
+            let pants = svg.getElementById("pants");
+
+            shirt.setAttribute("fill", "yellow");
+            pants.setAttribute("fill", "green");
+        }
+    });
     return (
+       
         <div className="avatar">
-            <img className="avatar-img" src="https://i.pinimg.com/originals/9a/3e/d1/9a3ed1afd74f706e7128a27d0bbf5532.jpg"></img>
+            <object ref={r=>obj=r} data={s} type="image/svg+xml" style={{width:'100%'}}>
+            </object>
         </div>
         )
 }
