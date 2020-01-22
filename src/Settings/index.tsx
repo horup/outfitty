e55882544b78c4ca6dd46c3ae45ddd6b92e73c9c
horup <sah@sah.dk>
2020-01-22 17:01:41 +0100

    added colors convert

--- src/Settings/index.tsx
+++ src/Settings/index.tsx
@@ -4,6 +4,7 @@ import ColorPicker from 'material-ui-color-picker';
 import { IState } from '..';
 import { useObserver } from 'mobx-react-lite';
 import { Typography, Card, CardContent } from '@material-ui/core';
+import * as convert from 'color-convert';
 
 const ColorChanger = ({color, text, onChange}:{color:string, text:string, onChange:(color:string)=>any})=>{
     return (
@@ -19,20 +20,28 @@ const ColorChanger = ({color, text, onChange}:{color:string, text:string, onChan
     )
 }
 
+const ColorStatus = ({state}:{state:IState})=>
+{
+    const num = 3;
+    const size = 50;
+    let p = 200;
+    
+    return useObserver(()=>(
+        <div>
+           No colors yet..
+        </div>
+    ))
+}
+
 export const Settings = ({state}:{state:IState})=>
 {
     return useObserver(()=>
-        <Card>
-            <CardContent>
-            <form noValidate autoComplete="off">
-                <ColorChanger color={state.shirtColor} text="Shirt Color" onChange={c=>state.shirtColor = c}/>
-                <ColorChanger color={state.blouseColor} text="Blouse Color" onChange={c=>state.blouseColor = c}/>
-
-                <ColorChanger color={state.pantsColor} text="Pants Color" onChange={c=>state.pantsColor = c}/>
-              
-                </form>
-            </CardContent>
-        </Card>
+        <form noValidate autoComplete="off">
+            <ColorChanger color={state.shirtColor} text="Shirt Color" onChange={c=>state.shirtColor = c}/>
+            <ColorChanger color={state.blouseColor} text="Blouse Color" onChange={c=>state.blouseColor = c}/>
+            <ColorChanger color={state.pantsColor} text="Pants Color" onChange={c=>state.pantsColor = c}/>
+            <ColorStatus state={state}/>
+        </form>
        
     )
 }
